5cc44625d66da80c6f40a007e27ab9b86c27f0e1
edwinaquino <github@edwinaquino.com>
2021-09-22 05:45:21 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'api_hook',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
